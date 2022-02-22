642b3758ca88ed3c5c42678891542069addb194b
brock <biswajitdekaofficial@gmail.com>
2022-02-22 10:12:44 +0530

    footer name spelling corrected

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
 		<div style={{ textAlign: 'center' }} className='text-muted pt-4 pb-3'>
 			<hr />
 			@ MCA 3rd Sem Minor Project, AEC, 2022 <br />
-			Biswajit Deka, Subashis Barua, Mridupawan Patowary, Sumonjit Kakoti
+			Biswajit Deka, Subashis Barua, Mridupaban Patowary, Sumonjit Kakoti
 			<hr />
 		</div>
 	)
